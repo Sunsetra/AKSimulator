30081e05fdfb6faa179e60914d3e41e12f05a7a7
B-R-D <xingxuanma@gmail.com>
2020-01-01 07:37:44 +0900

    Ver.0.0.3 增加装饰建筑类；

---
+++ modules/buildings/Decoration.ts
@@ -0,0 +1,19 @@
+import { FrontSide } from '../../node_modules/three/src/constants.js';
+import { Mesh } from '../../node_modules/three/src/objects/Mesh.js';
+import { BuildingInfo } from '../MapInfo.js';
+import Building from './Building.js';
+
+
+class Decoration extends Building {
+  constructor(mesh: Mesh, info: BuildingInfo) {
+    super(mesh, info);
+    if (Array.isArray(mesh.material)) {
+      mesh.material.forEach((mat) => { mat.side = FrontSide; });
+    } else { mesh.material.side = FrontSide; }
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
+  }
+}
+
+
+export default Decoration;
